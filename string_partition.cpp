45f80d7af0ba782512d45e3716e8cc5cdd3503f8
Archit Jain <arcyjain2002@gmail.com>
2022-07-09 22:40:58 +0530

    String partition

---
+++ string_partition.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int count = 0;
+bool check(string str, int M)
+{
+    if (((str[str.length() - 1] - '0') % 2 == 1) && (((str[0] - '0') % 2) == 0) && str.length() >= M)
+    {
+        return true;
+    }
+    else
+        return false;
+}
+
+long long part(int N, int M, int K, string S, int j)
+{
+    if (K == 0)
+    {
+        return count = count + 1;
+    }
+
+    for (int k = j; k < S.length() - j; k++)
+    {
+        string str1 = S.substr(0, k);
+        string rem = S.substr(k, S.length() - 1);
+        if (check(str1, j) == true && check(rem, j) == true)
+        {
+
+            cout << str1 << " " << rem << endl;
+            count = count + part(rem.length(), M, K - 1, rem, j); //+ part(rem.length(), M, K - 1, rem, j + 1);
+        }
+    }
+    return count;
+}
+
+long long solve(int N, int M, int K, string S)
+{
+    return part(N, M, K - 1, S, M);
+}
+
+int main(int argc, char const *argv[])
+{
+    string S = "232387421";
+    int N = S.length();
+    int M = 2;
+    int K = 3;
+    cout << solve(N, M, K, S);
+    return 0;
+}
