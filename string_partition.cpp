b488e24c42cc02300b198f2ce8093a717edd4cdb
Archit Jain <arcyjain2002@gmail.com>
2022-07-10 00:56:15 +0530

    solved

--- string_partition.cpp
+++ string_partition.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-int count = 0;
 bool check(string str, int M)
 {
     if (((str[str.length() - 1] - '0') % 2 == 1) && (((str[0] - '0') % 2) == 0) && str.length() >= M)
@@ -11,14 +10,15 @@ bool check(string str, int M)
         return false;
 }
 
-long long part(int N, int M, int K, string S, int j)
+void part(int N, int M, int K, string S, int j, int &counting)
 {
     if (K == 0)
     {
-        return count = count + 1;
+        counting = counting + 1;
+        return;
     }
 
-    for (int k = j; k < S.length() - j; k++)
+    for (int k = j; k <= S.length() - j; k++)
     {
         string str1 = S.substr(0, k);
         string rem = S.substr(k, S.length() - 1);
@@ -26,23 +26,31 @@ long long part(int N, int M, int K, string S, int j)
         {
 
             cout << str1 << " " << rem << endl;
-            count = count + part(rem.length(), M, K - 1, rem, j); //+ part(rem.length(), M, K - 1, rem, j + 1);
+            part(rem.length(), M, K - 1, rem, j, counting);
         }
     }
-    return count;
+    return;
 }
 
 long long solve(int N, int M, int K, string S)
 {
-    return part(N, M, K - 1, S, M);
+    int counting = 0;
+    part(N, M, K - 1, S, M, counting);
+    return counting;
 }
 
 int main(int argc, char const *argv[])
 {
-    string S = "232387421";
+    // string S = "232387421";
+    // int N = S.length();
+    // int M = 2;
+    // int K = 3;
+
+    string S = "2453231211";
     int N = S.length();
-    int M = 2;
+    int M = 3;
     int K = 3;
     cout << solve(N, M, K, S);
+
     return 0;
 }
