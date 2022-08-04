060a885ee887f0b14190314939cb1e9d7a3a9e7f
Archit Jain <arcyjain2002@gmail.com>
2022-08-04 23:03:15 +0530

    Sort by set bit count

---
+++ Sorting and Sorting/13.Sort_by_set_bit_count.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    static bool cmp(int a, int b)
+    {
+        unsigned long long count1 = 0;
+        unsigned long long count2 = 0;
+        while (a)
+        {
+
+            count1 = count1 + (a & 1);
+            a = a >> 1;
+        }
+        while (b)
+        {
+
+            count2 = count2 + (b & 1);
+            b = b >> 1;
+        }
+
+        return count1 > count2;
+    }
+
+    void sortBySetBitCount(int arr[], int n)
+    {
+        stable_sort(arr, arr + n, cmp);
+        return;
+    }
