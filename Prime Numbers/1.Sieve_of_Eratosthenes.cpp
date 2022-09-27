d52b0b20d7a148033af573a15b46af0966dba357
Archit Jain <arcyjain2002@gmail.com>
2022-09-28 00:54:06 +0530

    Sieve of Eratosthenes

---
+++ Prime Numbers/1.Sieve_of_Eratosthenes.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution
+{
+public:
+    vector<int> sieveOfEratosthenes(int N)
+    {
+        bool prime[N + 1];
+        memset(prime, true, sizeof(prime));
+
+        for (int p = 2; p * p < N; p++)
+        {
+            if (prime[p] == true)
+            {
+                for (int i = p * p; i <= N; i = i + p)
+                {
+                    prime[i] = false;
+                }
+            }
+        }
+        vector<int> vec;
+
+        for (int i = 2; i <= N; i++)
+        {
+            if (prime[i] == true)
+            {
+                vec.push_back(i);
+            }
+        }
+        return vec;
+    }
+};
