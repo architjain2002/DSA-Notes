888388c36ad1a4dc5e2bb57e70241d394ea8a5c7
Archit Jain <arcyjain2002@gmail.com>
2022-09-29 17:29:47 +0530

    Program to find a given number is power of 2

---
+++ Bit_manipulation/6.1.Program_to_find_a_given_number_is_power_of_2.cpp
@@ -0,0 +1,20 @@
+// O(n) solution - naive solution
+
+bool isPowerOfTwo(int n)
+{
+    int cnt = 0;
+    while (n > 0)
+    {
+        if ((n & 1) == 1)
+        {
+            cnt++;
+        }
+        n = n >> 1;
+    }
+
+    if (cnt == 1)
+    {
+        return true;
+    }
+    return false;
+}
