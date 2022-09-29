2855a8217de0625f2b78380a90536a5b4449711b
Archit Jain <arcyjain2002@gmail.com>
2022-09-29 16:27:39 +0530

    Count total Number of bits from 1 to N O(logn) solution

---
+++ Bit_manipulation/5.Count_total_number_of_bits_from_1_to_N_O(logn).cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    int nearestTwoPowerBit(int n)
+    {
+        int count = 0;
+
+        while ((1 << count) <= n)
+        {
+            count++;
+        }
+
+        return count - 1;
+    }
+
+    int countBits(int N)
+    {
+        if (N <= 0)
+        {
+            return 0;
+        }
+        if (N == 1)
+        {
+            return 1;
+        }
+        int x = nearestTwoPowerBit(N);
+        // cout << x;
+        return x * pow(2, x - 1) + (N - pow(2, x) + 1) + countBits(N - pow(2, x));
+    }
+};
