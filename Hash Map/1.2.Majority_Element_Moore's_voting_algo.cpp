ca7bc697216ba3b880095ee237af1fec21548909
Archit Jain <arcyjain2002@gmail.com>
2022-11-01 16:07:58 +0530

    Majority Element Moores voting algo

---
+++ Hash Map/1.2.Majority_Element_Moore's_voting_algo.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+
+        int ind = 0;
+        int count = 1;
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[ind] == nums[i])
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+
+            if (count == 0)
+            {
+                ind = i;
+                count = 1;
+            }
+        }
+        return nums[ind];
+    }
+};
